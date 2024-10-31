4e25c46922d61b1c718fa26bf971efa7ce2da2c3
Pedro Coelho <12400653@aluno.cotemig.com.br>
2024-10-31 20:22:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let currentIndex = 0;
+    const images = document.querySelectorAll('.carousel-image');
+    const totalImages = images.length;
+
+    function showNextImage() {
+        // Remove a classe 'active' de todas as imagens
+        images.forEach(img => img.classList.remove('active'));
+
+        // Calcula o próximo índice
+        currentIndex = (currentIndex + 1) % totalImages;
+
+        // Adiciona a classe 'active' à imagem atual
+        images[currentIndex].classList.add('active');
+
+        // Move o carrossel para a imagem atual
+        const offset = -currentIndex * 100; // Move a largura de uma imagem para a esquerda
+        document.querySelector('.carousel-images').style.transform = `translateX(${offset}%)`;
+    }
+
+    // Inicializa o carrossel
+    showNextImage(); // Mostra a primeira imagem
+    setInterval(showNextImage, 3000); // Alterna as imagens a cada 3 segundos
+});
+
+
+window.onscroll = function() {
+    const btn = document.getElementById("back-to-top-btn");
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
+};
+
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' 
+    });
+}
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
